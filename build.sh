18fa799e4b30915cfef53c4b9ef779b8d627d427
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-02-27 02:29:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ MODULES=0
 LINKER=ld.lld
 
 # Clean source prior building. 1 is NO(default) | 0 is YES
-INCREMENTAL=1
+INCREMENTAL=0
 
 # Push ZIP to Telegram. 1 is YES | 0 is NO(default)
 PTTG=0
@@ -90,7 +90,7 @@ PTTG=0
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
-DEF_REG=0
+DEF_REG=1
 
 # Files/artifacts
 FILES=Image.gz-dtb
